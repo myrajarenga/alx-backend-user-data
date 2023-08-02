93fb5f06c461368c83832097280a45818041748a
myrajarenga <myrajarenga1234@gmail.com>
2023-08-02 06:38:07 -0700

    filter_datum function is to obfuscate specific fields in a log message.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,12 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str,
+        message: str, separator: str
+        ) -> str:
     """function onbfuscate specific fields in log mesage"""
-    return re.sub(fr'({"|".join(map(re.escape, fields))})=([^{separator}]*)', fr'\1={redaction}', message)
+    return re.sub(
+            fr'({"|".join(map(re.escape, fields))})=([^{separator}]*)',
+            fr'\1={redaction}', message
+            )
