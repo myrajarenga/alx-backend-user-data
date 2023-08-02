3f1e1d906165887e6fec5f214ae750ee8abdc8e3
myrajarenga <myrajarenga1234@gmail.com>
2023-08-02 08:23:07 -0700

    connecting to secure databse"

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,11 +5,14 @@ filter_datum using regex
 import re
 from typing import List
 import logging
+import csv
+import os
+import mysql.connector
 
 
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
- 
+
     REDACTION = "***"
     SEPARATOR = ";"
 
@@ -31,3 +34,39 @@ def filter_datum(
             fr'({"|".join(map(re.escape, fields))})=([^{separator}]*)',
             fr'\1={redaction}', message
             )
+
+
+def get_logger():
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    pii_fields = ('name', 'email', 'phone', 'ssn', 'password')
+    formatter = RedactingFormatter(pii_fields)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+def get_db():
+    db_username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
+    db_password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
+    db_host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+
+    try:
+        db_connection = mysql.connector.connect(
+            host=db_host,
+            user=db_username,
+            password=db_password,
+            database=db_name
+        )
+        return db_connection
+    except mysql.connector.Error as e:
+        print(f"Error connecting to the database: {e}")
+        return None
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -5,6 +5,29 @@ Main file
 import logging
 import re
 
+
+get_db = __import__('filtered_logger').get_db
+
+try:
+    db = get_db()
+    if db:
+        cursor = db.cursor()
+        cursor.execute("SELECT COUNT(*) FROM users;")
+        for row in cursor:
+            print(row[0])
+        cursor.close()
+        db.close()
+    else:
+         print("Database connection failed.")
+except Exception as e:
+        print(f"Error: {e}")
+
+get_logger = __import__('filtered_logger').get_logger
+PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+
+print(get_logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+
 RedactingFormatter = __import__('filtered_logger').RedactingFormatter
 
 message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
