588fb2805e633ec12793ade9e711f0a1782f1d53
myrajarenga <myrajarenga1234@gmail.com>
2023-08-03 06:09:55 -0700

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Password Encryption and Validation Module
+"""
+import bcrypt
+
+
+def hash_password(password:str) -> bytes:
+    """
+    Hashes the input password using bcrypt.
+
+    Args:
+        password: The password to be hashed.
+
+    Returns:
+        A salted, hashed password as a byte string.
+    """
+    password_bytes = password.encode('utf-8')
+
+    #generating  salt and has the password using bcrypt
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password
+
+
+if __name__ == "__main__":
+    password = "MyAmazingPassw0rd"
+    print(hash_password(password))
+    print(hash_password(password))
+
