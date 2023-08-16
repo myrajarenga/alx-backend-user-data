45a757895fdf159d03f03db92c184344fdcc97e3
myrajarenga <myrajarenga1234@gmail.com>
2023-08-16 03:41:18 -0700

    implimenting the DB.update_user method that takes as argument a required user_id integer and arbitrary keyword arguments, and returns None

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,3 +53,17 @@ class DB:
         if not user:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        function to update use in db
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+
+                setattr(user, key, value)
+
+        self._session.commit()
+        return None
