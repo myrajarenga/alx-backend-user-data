64864da27192240e637f053eed9c7370500fba2e
myrajarenga <myrajarenga1234@gmail.com>
2023-08-16 05:02:28 -0700

    implimenting methods to get user from session id, destroy session, rget reset password tocken and update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,3 +73,53 @@ class Auth:
         else:
             user.session_id = _generate_uuid()
             return user.session_id
+
+     def get_user_from_session_id(self, session_id: str) -> User:
+        """_summary_
+        method to get user from session id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
+
+    def destroy_session(self, user_id: str) -> None:
+        """_summary_
+        method to destroy session
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """_summary_
+        Method to resset password
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """_summary_
+        method to uupdate password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
